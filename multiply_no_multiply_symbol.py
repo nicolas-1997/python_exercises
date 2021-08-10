f3ebcb0d420c20cc8066a96e384229e54b98cf3b
Nicolas Cabrera <nicocabrera.0212@gmail.com>
2021-08-10 17:12:03 -0300

    we create an algorithm to multiply int numbers without the multiply sign

---
+++ multiply_no_multiply_symbol.py
@@ -0,0 +1,19 @@
+def multiply_no_multiply_symbol(a,b : int):
+    x = 0
+    for _ in range(b):
+       x += a
+
+    return x
+
+def run(): 
+    try:
+        a = int(input("Enter the first number to multiply: "))
+        b = int(input("Enter the second number to multiply: "))
+    except:
+        print("You can only use number")
+
+    print(f"{a} x {b} = ",multiply_no_multiply_symbol(a,b))
+
+
+if __name__ == "__main__":
+    run()
