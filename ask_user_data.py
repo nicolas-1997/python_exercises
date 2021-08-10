13976cd6d627e01a40f2685c8aa46da7c2eda124
Nicolas Cabrera <nicocabrera.0212@gmail.com>
2021-08-10 20:09:05 -0300

    minimal changes and create a function that sums the values

--- ask_user_data.py
+++ ask_user_data.py
@@ -1,12 +1,19 @@
+def add_elements(lista):
+    result = 0
+    for element in lista:
+        result += element
+    return result
+
 def run():
     lista = []
-    print("Enter only numbers and to exit write enough")
+    print("Enter only numbers and to exit write enough or x")
     while True:
         entry = input("Enter a numer: ")
         try:
             data = entry.lower()
-            if data == "enough":
+            if data == "enough" or data == "x":
                 print("Your values",lista)
+                print("the sum of your entered values ​​is: ",add_elements(lista))
                 break
             else:
                 try:
